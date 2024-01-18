4ead9add5eb0222e6a807dd96b51f829c8bf332f
lmhjin2 <lmhjin2@naver.com>
2024-01-18 15:42:09 +0900

    2024-01-18 15:42

---
+++ keras/keras30_gpu_test02_california.py
@@ -0,0 +1,120 @@
+import matplotlib.pyplot as plt
+import numpy as np
+import pandas as pd
+from keras.models import Sequential
+from keras.layers import Dense, Dropout
+from keras.callbacks import EarlyStopping, ModelCheckpoint
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import r2_score, mean_squared_error,mean_squared_log_error,mean_absolute_error
+from sklearn.datasets import fetch_california_housing
+from sklearn.preprocessing import MinMaxScaler, StandardScaler, MaxAbsScaler, RobustScaler
+plt.rcParams['font.family'],"Malgun Gothic"
+plt.rcParams['axes.unicode_minus']=False
+
+#1 
+datasets = fetch_california_housing()
+x = datasets.data
+y = datasets.target
+
+x_train, x_test, y_train, y_test = train_test_split(
+    x, y, train_size = 0.85, random_state=0 )
+
+# scaler = MinMaxScaler()
+# scaler = StandardScaler()
+scaler = MaxAbsScaler()
+# scaler = RobustScaler()
+
+scaler.fit(x_train)
+x_train = scaler.transform(x_train)
+x_test = scaler.transform(x_test)
+
+#2
+model = Sequential()
+model.add(Dense(16, input_dim = 8))
+model.add(Dense(32))
+model.add(Dropout(0.2))
+model.add(Dense(15))
+model.add(Dense(30))
+model.add(Dropout(0.2))
+model.add(Dense(8))
+model.add(Dense(1))
+
+#3
+import datetime
+
+date = datetime.datetime.now()
+date = date.strftime("%m%d_%H%M")   # 월일_시분
+
+path = "c:/_data/_save/MCP/k28/02/"
+filename = "{epoch:04d}-{val_loss:.4f}.hdf5"
+filepath = "".join([path, 'k28_', date, '_', filename])
+model.compile(loss='mse',optimizer='adam',
+              metrics = ['mse','msle', 'mae'])
+es = EarlyStopping(monitor='val_loss', mode='min',
+                   patience = 300, verbose = 1,
+                   restore_best_weights=True)
+mcp = ModelCheckpoint(monitor='val_loss', mode = 'auto', verbose = 1,
+                      save_best_only=True,
+                      filepath=filepath)
+hist = model.fit(x_train, y_train, epochs=3000,
+                 batch_size = 301, validation_split=0.2,
+                 verbose = 2, callbacks=[es, mcp])
+
+#4
+loss = model.evaluate(x_test, y_test)
+y_predict = model.predict(x_test)
+r2 = r2_score(y_test, y_predict)
+
+def RMSE(y_test, y_predict):
+    return np.sqrt(mean_squared_error(y_test, y_predict))
+def RMSLE(y_test, y_predict):
+    return np.sqrt(mean_squared_log_error(y_test,y_predict))
+
+rmse = RMSE(y_test, y_predict)
+# rmsle = RMSLE(y_test, y_predict)
+# print("음수갯수 :", y[y['count']<0].count())  ## 진짜 중요함 ##
+
+print('loss:', loss)
+print('RMSE:', rmse)
+# print('RMSLE:', rmsle)
+print('R2:',r2)
+'''
+plt.figure(figsize=(9,6))
+plt.plot(hist.history['loss'], color = 'red',
+         label = 'loss', marker = '.')
+plt.plot(hist.history['val_loss'], color = 'blue',
+         label = 'val_loss', marker = '.')
+plt.legend(loc='upper right')
+plt.title('캘리포니아 로스')
+plt.xlabel('epochs')
+plt.ylabel('val_loss')
+plt.grid()
+plt.show()
+'''
+
+# loss: [0.5937129259109497, 0.5937129259109497, 0.05482317879796028, 0.5589895248413086]
+# RMSE: 0.7705276989046251
+# R2: 0.5519150654481986
+
+# scaler = MinMaxScaler()
+# loss: [0.5377083420753479, 0.5377083420753479, 0.052736494690179825, 0.5330697298049927]
+# RMSE: 0.733286021331258
+# R2: 0.5941826188806718
+
+# scaler = StandardScaler()
+# loss: [0.5548639893531799, 0.5548639893531799, 0.05236700922250748, 0.5384661555290222]
+# RMSE: 0.7448919710115381
+# R2: 0.5812349600575786
+
+# scaler = MaxAbsScaler()
+# loss: [0.5390101075172424, 0.5390101075172424, 0.052398569881916046, 0.5343481302261353]
+# RMSE: 0.7341730965529952
+# R2: 0.5932001692557324
+
+# scaler = RobustScaler()
+# loss: [1.26486074924469, 1.26486074924469, 0.05075812339782715, 0.5429605841636658]
+# RMSE: 1.1246603976563418
+# R2: 0.045388554455149444
+
+
+# MixMaxScaler
---
+++ keras/keras30_gpu_test06_caner.py
@@ -0,0 +1,132 @@
+import numpy as np
+import pandas as pd
+from sklearn.datasets import load_breast_cancer
+from keras.models import Sequential
+from keras.layers import Dense, Dropout
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import r2_score, accuracy_score
+from keras.callbacks import EarlyStopping, ModelCheckpoint
+import matplotlib.pyplot as plt
+
+plt.rcParams['font.family']='Malgun Gothic'
+plt.rcParams['axes.unicode_minus']=False
+
+#1 데이터
+datasets = load_breast_cancer()
+# print(datasets)
+# print(datasets.DESCR)   # (569, 30)
+# print(datasets.feature_names)
+x = datasets.data
+y = datasets.target
+# numpy
+print(np.unique(y)) # [ 0 1 ]  
+
+print(np.unique(y, return_counts=True)) # (array([0, 1]), array([212, 357], dtype=int64))
+
+# pandas 넷다똑같음
+# print(pd.DataFrame(y).value_counts())
+# print(pd.Series(y).value_counts())
+# print(pd.Series.value_counts(y))
+print(pd.value_counts(y))
+
+# 1    357
+# 0    212
+
+
+# print(x.shape, y.shape) # (569, 30) (569,)
+x_train, x_test, y_train, y_test = train_test_split(
+    x, y, train_size = 0.85, random_state = 1 )
+
+from sklearn.preprocessing import MinMaxScaler, MaxAbsScaler
+from sklearn.preprocessing import StandardScaler, RobustScaler
+
+# scaler = MinMaxScaler()
+# scaler = StandardScaler()
+# scaler = MaxAbsScaler()
+scaler = RobustScaler()
+
+scaler.fit(x_train)
+x_train = scaler.transform(x_train)
+x_test = scaler.transform(x_test)
+
+
+#2 
+model = Sequential()
+model.add(Dense(110, input_dim = 30))
+model.add(Dropout(0.2))
+model.add(Dense(80))
+model.add(Dropout(0.2))
+model.add(Dense(50))
+model.add(Dropout(0.2))
+model.add(Dense(20))
+model.add(Dense(1, activation = 'sigmoid'))
+
+#3
+import datetime
+
+date = datetime.datetime.now()
+date = date.strftime("%m%d_%H%M")   # 월일_시분
+
+path1 = "c:/_data/_save/MCP/k28/06/"
+filename = "{epoch:04d}-{val_loss:.4f}.hdf5"
+filepath = "".join([path1, 'k28_', date, '_', filename])
+
+model.compile(loss = 'binary_crossentropy', optimizer='adam',
+              metrics=['accuracy','mse','mae'])
+es = EarlyStopping(monitor = 'val_loss', mode = 'auto',
+                   patience = 100, verbose = 1,
+                   restore_best_weights=True)
+mcp = ModelCheckpoint(monitor='val_loss', mode='auto',
+                      verbose = 1, save_best_only=True,
+    filepath=filepath)
+hist = model.fit(x_train, y_train, epochs = 1000,
+                 batch_size = 105, validation_split = 0.13,
+                 verbose = 2, callbacks=[es, mcp])
+# loss: 0.09776605665683746
+# r2: 0.5982901288073281
+
+#4 
+loss = model.evaluate(x_test, y_test)   # x_test를 넣어서 predict한 값을 y_test 와 비교.
+y_predict = model.predict(x_test)
+r2 = r2_score(y_test, y_predict)
+# print("======================================================================================")
+# print(y_test)
+# print("======================================================================================")
+# print(y_predict)
+# print("======================================================================================")
+
+def ACC(y_test, y_predict):
+    return accuracy_score(y_test, y_predict)
+
+acc = ACC(y_test, np.round(y_predict))
+
+
+print('loss:', loss)
+print('acc: ', acc)
+print('r2:', r2)
+
+# loss: [0.20226573944091797, 0.9534883499145508, 0.046718303114175797, 0.06178978830575943]
+# r2: 0.8080396679605841
+
+
+# scaler = MinMaxScaler()
+# loss: [0.08510842174291611, 0.9651162624359131, 0.02643170952796936, 0.05430173873901367]
+# acc:  0.9651162790697675
+# r2: 0.8894293325067316
+
+# scaler = StandardScaler()
+# loss: [0.15428347885608673, 0.9418604373931885, 0.044408977031707764, 0.0717775747179985]
+# acc:  0.9418604651162791
+# r2: 0.8142258012453736
+
+# scaler = MaxAbsScaler()
+# loss: [0.11676334589719772, 0.9534883499145508, 0.03654831647872925, 0.06537999957799911]
+# acc:  0.9534883720930233
+# r2: 0.8471089766129697
+
+# scaler = RobustScaler()
+# loss: [0.07344205677509308, 0.9651162624359131, 0.02196618542075157, 0.054660264402627945]
+# acc:  0.9651162790697675
+# r2: 0.9081097883593787
+
+# RobustScaler
