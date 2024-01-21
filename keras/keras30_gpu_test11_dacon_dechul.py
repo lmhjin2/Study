1d00a137b8fb3fadf9b9171a9b10fe6778aade53
lmhjin2 <155138597+lmhjin2@users.noreply.github.com>
2024-01-21 17:59:44 +0900

    2024-01-21 17:59

--- keras/keras30_gpu_test11_dacon_dechul.py
+++ keras/keras30_gpu_test11_dacon_dechul.py
@@ -122,7 +122,7 @@ y_submit = np.argmax(y_submit, axis=1)
 y_submit = le_grade.inverse_transform(y_submit)
 
 submission_csv['대출등급'] = y_submit
-submission_csv.to_csv(path + "submission_0119_rbs1.csv", index=False)
+submission_csv.to_csv(path + "submission_0121_rbs1.csv", index=False)
 
 acc = accuracy_score(y_test, y_predict)
 f1 = f1_score(y_test, y_predict, average = 'macro') # [None, 'micro', 'macro', 'weighted'] 중에 하나
