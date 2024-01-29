0f7ea8e4627f004cd6f6682479a02fe891d0df83
lmhjin2 <lmhjin2@naver.com>
2024-01-29 20:09:29 +0900

    2024-01-29 20:09

--- keras/keras47_split.py
+++ keras/keras47_split.py
@@ -9,15 +9,15 @@ def split_x(dataset, size):
         subset = dataset[i : (i+size)]  # dataset에서 i 부터 i+5, 실질적으로는 i+4까지 subset 이라고 하기
         aaa.append(subset)          # subset에 담긴걸 aaa에 담기
       # aaa.append(dataset[i : (i+size)])  / # dataset에서 i 부터 i+5, 실질적으로는 i+4까지 aaa에 담기
-    return np.array(aaa)   # 외주 맡긴거 돌려받기. 안주면 내가 못쓰자너
+    return np.array(aaa)   # 외주 맡긴거 돌려받기. 안주면 내가 못쓰자너. 쿠팡맨.
 
 bbb = split_x(a, size)
 # print(bbb)
 # print(bbb.shape)  # (6, 5)
 
-x = bbb[:,:-1]  # "bbb" 배열에서 마지막 열을 제외한 모든 열을 가져와 "x"라는 이름의 새 배열에 저장.
-    # 첫번째 : 은 행. 즉, 모든행. / 두번째 : 은 열. : -1 이니까 마지막 하나 빼고 다
-y = bbb[:,-1]   # "bbb" 배열에서 마지막 열만 가져와 "y"라는 이름의 새 배열에 저장.
+x = bbb[ : , : -1 ]  # "bbb" 배열에서 마지막 열을 제외한 모든 열을 가져와 "x"라는 이름의 새 배열에 저장.
+    # 첫번째 : 은 행. 비어있으니 모든행 선택. / 두번째 : 은 열. : -1 이니까 마지막 하나 빼고 다.
+y = bbb[ : , -1 ]   # "bbb" 배열에서 마지막 열만 가져와 "y"라는 이름의 새 배열에 저장.
     # 첫번째 :은 행. 모든행/ 두번째 -1 은 인덱스 번호 [-1]. 마지막 하나만 갖고오기
 # print(x, y)
 # print(x.shape, y.shape)  # (6, 4) (6,)
