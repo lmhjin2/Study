6d2cee82f25b6330feb93507caff0ce1411b0c2b
lmhjin2 <lmhjin2@naver.com>
2024-01-30 11:07:17 +0900

    2024-01-30 11:07

---
+++ keras/keras48_split3_LSTM2.py
@@ -0,0 +1,87 @@
+import numpy as np
+import pandas as pd
+from keras.models import Sequential
+from keras.layers import Dense, LSTM, Dropout
+from keras.callbacks import EarlyStopping
+
+a = np.array(range(1,101))   # a = (1~100)
+x_predict = np.array(range(96, 106)) # (96~105) 10개
+
+size = 5    # x데이터는 4개 y데이터는 1개
+
+def split_x(dataset, size):
+    aaa = []            # aaa list만들기
+    for i in range(len(dataset) - size + 1):        # dataset - 5 + 1
+        subset = dataset[i : (i+size)]  # dataset에서 i 부터 i+5, 실질적으로는 i+4까지 subset 이라고 하기
+        aaa.append(subset)          # subset에 담긴걸 aaa에 담기
+      # aaa.append(dataset[i : (i+size)])  / # dataset에서 i 부터 i+5, 실질적으로는 i+4까지 aaa에 담기
+    return np.array(aaa)   # 외주 맡긴거 돌려받기. 안주면 내가 못쓰자너. 쿠팡맨.
+
+bbb = split_x(a, size)
+# print(bbb)
+# print(bbb.shape)  # (6, 5)
+
+x = bbb[ : , : -1 ]  # "bbb" 배열에서 마지막 열을 제외한 모든 열을 가져와 "x"라는 이름의 새 배열에 저장.
+    # 첫번째 : 은 행. 비어있으니 모든행 선택. / 두번째 : 은 열. : -1 이니까 마지막 하나 빼고 다.
+y = bbb[ : , -1 ]   # "bbb" 배열에서 마지막 열만 가져와 "y"라는 이름의 새 배열에 저장.
+    # 첫번째 :은 행. 모든행/ 두번째 -1 은 인덱스 번호 [-1]. 마지막 하나만 갖고오기
+
+# 모델 구성 및 평가 예측
+
+xxx = split_x(x_predict, size)
+predict = xxx[:,:-1]
+predict = np.append(predict, [[102,103,104,105]], axis=0)
+# print(predict)    # (7, 4)
+# print(x.shape, y.shape) # (96,4) (96)
+x = x.reshape(-1,2,2)
+predict = predict.reshape(-1,2,2)
+
+# #2
+model = Sequential()
+model.add(LSTM(32, input_shape = (2,2), return_sequences=True, activation='swish'))
+model.add(LSTM(10, activation='swish'))
+model.add(Dense(32, activation='swish'))
+model.add(Dense(15, activation='swish'))
+model.add(Dense(48, activation='swish'))
+model.add(Dense(26, activation='swish'))
+model.add(Dense(12, activation='swish'))
+model.add(Dense(1))
+
+model.summary()
+
+#3
+model.compile(loss='mse', optimizer='adam')
+es = EarlyStopping(monitor='loss', mode='auto',
+                   patience = 200, verbose=1, restore_best_weights=True)
+model.fit(x, y, epochs = 1000, batch_size=32, callbacks=[es])
+
+#4
+loss = model.evaluate(x, y)
+y_predict = model.predict(predict)
+print('loss', loss)
+print(np.round(y_predict),"\n", y_predict)
+
+# [[ 96  97  98  99]    # 100
+#  [ 97  98  99 100]    # 101
+#  [ 98  99 100 101]    # 102
+#  [ 99 100 101 102]    # 103
+#  [100 101 102 103]    # 104
+#  [101 102 103 104]    # 105
+#  [102 103 104 105]]   # 106
+
+
+# [[100.]
+#  [101.]
+#  [102.]
+#  [103.]
+#  [104.]
+#  [105.]
+#  [106.]]
+
+#  [[ 99.996124]
+#  [100.99605 ]
+#  [101.99603 ]
+#  [102.99603 ]
+#  [103.99609 ]
+#  [104.99618 ]
+#  [105.99629 ]]
