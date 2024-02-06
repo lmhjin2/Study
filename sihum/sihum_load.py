f05c3d4e34113037b40702c83d27fb24429517d8
lmhjin2 <lmhjin2@naver.com>
2024-02-06 18:38:07 +0900

    2024-02-06 18:38

--- sihum/sihum_load.py
+++ sihum/sihum_load.py
@@ -87,7 +87,7 @@ x2_test = x2_test.reshape(141,10,8)
 x1_predict = x1_predict.reshape(10,10,8)
 x2_predict = x2_predict.reshape(10,10,8)
 
-model = load_model(path + 'sihum_0206_1207_0133-5182.92.hdf5')
+model = load_model(path + 'sihum_0206_1648_0174-6349.60.hdf5')
 
 loss = model.evaluate([x1_test, x2_test],[y1_test, y2_test])
 
