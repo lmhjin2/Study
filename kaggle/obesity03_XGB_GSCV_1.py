76b5bd925e9fe828e6c51e49331dbad4b1a8e601
lmhjin2 <155138597+lmhjin2@users.noreply.github.com>
2024-02-13 16:31:22 +0900

    2024-02-13 16:31

--- kaggle/obesity03_XGB_GSCV_1.py
+++ kaggle/obesity03_XGB_GSCV_1.py
@@ -68,7 +68,7 @@ from sklearn.metrics import accuracy_score, r2_score
 from xgboost import XGBClassifier
 from sklearn.multioutput import MultiOutputClassifier
 
-parameters = [{'seed':range(740,750,1)}]
+parameters = [{'seed':range(750,760,1)}]
 
 
 model = GridSearchCV(XGBClassifier(n_estimators = 1000 , 
