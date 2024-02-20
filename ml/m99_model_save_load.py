d648f0e06ed278f0879704998086329295be206a
lmhjin2 <lmhjin2@naver.com>
2024-02-20 16:16:26 +0900

    2024-02-20 16:16

--- ml/m99_model_save_load.py
+++ ml/m99_model_save_load.py
@@ -20,8 +20,10 @@ model = pickle.load(open(path + 'm39_pickle1_save.dat', 'rb'))  # rb = read bina
 
 ##############################################################################
 "## XGBoost 내장함수"
+import xgboost as xgb
 # 모델 저장
 model.save_model('xgboost_model.xgb')
 
 # 모델 불러오기
 loaded_model = xgb.Booster(model_file='xgboost_model.xgb')
+
