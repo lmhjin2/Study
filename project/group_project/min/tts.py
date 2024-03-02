c7bfa8f1ba3ccc32be293f2195ed7616d06adcb1
lmhjin2 <lmhjin2@naver.com>
2024-03-02 19:38:20 +0900

    2024-03-02 19:38

--- project/group_project/min/tts.py
+++ project/group_project/min/tts.py
@@ -2,9 +2,9 @@ from gtts import gTTS
 import os
 
 # 텍스트 입력
-text = ""
+text = "a man is swinging a tennis racket at a tennis ball"
 
 # gTTS를 사용하여 음성 파일 생성
 tts = gTTS(text=text, lang='en')
 
-tts.save("C:/Users/AIA/Desktop/temp/tts.mp3")
+tts.save("C:/Users/AIA/Desktop/temp/000000326379.mp3")
