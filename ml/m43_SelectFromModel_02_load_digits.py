0997e1014bd07911c026cf95b70964f50abcdf95
lmhjin2 <lmhjin2@naver.com>
2024-03-05 20:11:44 +0900

    2024-03-05 20:11

--- ml/m43_SelectFromModel_02_load_digits.py
+++ ml/m43_SelectFromModel_02_load_digits.py
@@ -73,38 +73,37 @@ print("acc: ", acc)
 # 걸린시간: 4.27 초
 
 ####################################################################################
-feature_importances_list = list(model.feature_importances_)
-# print(feature_importances_list)
-feature_importances_list_sorted = sorted(feature_importances_list)
-# print(feature_importances_list_sorted)  # 0번이제일 낮고 29번이 제일높음 (총 컬럼 30개)
-drop_feature_idx_list = [feature_importances_list.index(feature) for feature in feature_importances_list_sorted]
-print(drop_feature_idx_list)
-
-result_dict = {}
-for i in range(len(drop_feature_idx_list)): # 1바퀴에 1개, 마지막 바퀴에 29개 지우기, len -1은 30개 다지우면 안돼서
-    drop_idx = drop_feature_idx_list[:i] # +1을 해준건 첫바퀴에 한개를 지워야 해서. 30개 시작 하고싶으면 i 만쓰고 위에 -1 지워주면됨
-    new_x_train = np.delete(x_train, drop_idx, axis = 1)
-    new_x_test = np.delete(x_test, drop_idx, axis=1)
-    print(new_x_train.shape, new_x_test.shape)
-    
-    model2 = XGBClassifier()
-    model2.set_params(early_stopping_rounds = 10, **parameters)
-    model2.fit(new_x_train,y_train,
-          eval_set=[(new_x_train,y_train), (new_x_test,y_test)],
-          verbose=0
-          )
-    new_result = model2.score(new_x_test,y_test)
-    print(f"{i}개 컬럼이 삭제되었을 때 Score: ",new_result)
-    result_dict[i] = new_result - results    # 그대로 보면 숫자가 비슷해서 구분하기 힘들기에 얼마나 변했는지 체크
-    
-    
-print(result_dict)
-
-
-
 
 # 12개 컬럼이 삭제되었을 때 Score:  0.9722222222222222
 # 13개 컬럼이 삭제되었을 때 Score:  0.9722222222222222
 # 16개 컬럼이 삭제되었을 때 Score:  0.9722222222222222
 # 23개 컬럼이 삭제되었을 때 Score:  0.9722222222222222
 # 24개 컬럼이 삭제되었을 때 Score:  0.9722222222222222
+
+print("="*50)
+thresholds = np.sort(model.feature_importances_)
+# print(thresholds)
+from sklearn.feature_selection import SelectFromModel
+
+for i in thresholds:
+    selection = SelectFromModel(model, threshold=i, prefit=False)
+    
+    select_x_train = selection.transform(x_train)
+    select_x_test = selection.transform(x_test)
+    # print(i, "\t변형된 x_train :", select_x_train.shape, "변형된 x_test :", select_x_test.shape)
+    # i 에는 thresholds 밑에 숫자가 하나씩 나오는데 그거 이상의 숫자를 가진 컬럼은 다 살아남고 / 그거 미만인 컬럼은 사라지는 구조
+    select_model = XGBClassifier()
+    select_model.set_params(
+        early_stopping_rounds=10, **parameters,
+    )
+    
+    select_model.fit(select_x_train, y_train,
+                     eval_set=[(select_x_train, y_train), (select_x_test, y_test)],
+                     verbose=0,)
+    select_y_predict = select_model.predict(select_x_test)
+    score = accuracy_score(y_test, select_y_predict)
+    print("Trech=%.3f, n=%d, ACC %.2f%%" % (i,select_x_train.shape[1], score*100))
+
+
+# Trech=0.010, n=41, ACC 97.50%
+
--- ml/m43_SelectFromModel_03_fetch_covtype.py
+++ ml/m43_SelectFromModel_03_fetch_covtype.py
@@ -67,56 +67,38 @@ y_predict = model.predict(x_test)
 acc = accuracy_score(y_test, y_predict)
 print("acc: ", acc)
 
-# model.score : 0.8850976308701153
-# 오름
-# 최적의 파라미터 :  {'n_estimators': 200, 'max_depth': 9, 'learning_rate': 0.3, 'lambda': 0.01, 'gamma': 0, 'alpha': 0.1}
-# best_score : 0.9590369417328016
-# 최적 튠 ACC: 0.9620233556792854
-# acc: [0.89492707 0.92414268 0.89462588 0.90959552 0.90843373]
-#  평균 acc: 0.9063
 # model.score: 0.9620233556792854
 # acc: 0.9620233556792854
 # 걸린시간: 465.52 초
-# model.score : 0.9583333333333334
-# 오름 
-# 최적의 파라미터 :  {'n_estimators': 300, 'max_depth': 3, 'learning_rate': 0.2, 'lambda': 0.1, 'gamma': 0, 'alpha': 0}
-# best_score : 0.9652076074332172
-# 최적 튠 ACC: 0.9611111111111111
-# acc: [0.875      0.90277778 0.90277778 0.875      0.97222222]
-#  평균 acc: 0.9056
-# model.score: 0.9611111111111111
-# acc: 0.9611111111111111
-# 걸린시간: 4.27 초
 
 ####################################################################################
-feature_importances_list = list(model.feature_importances_)
-# print(feature_importances_list)
-feature_importances_list_sorted = sorted(feature_importances_list)
-# print(feature_importances_list_sorted)  # 0번이제일 낮고 29번이 제일높음 (총 컬럼 30개)
-drop_feature_idx_list = [feature_importances_list.index(feature) for feature in feature_importances_list_sorted]
-print(drop_feature_idx_list)
-
-result_dict = {}
-for i in range(len(drop_feature_idx_list)): # 1바퀴에 1개, 마지막 바퀴에 29개 지우기, len -1은 30개 다지우면 안돼서
-    drop_idx = drop_feature_idx_list[:i] # +1을 해준건 첫바퀴에 한개를 지워야 해서. 30개 시작 하고싶으면 i 만쓰고 위에 -1 지워주면됨
-    new_x_train = np.delete(x_train, drop_idx, axis = 1)
-    new_x_test = np.delete(x_test, drop_idx, axis=1)
-    print(new_x_train.shape, new_x_test.shape)
-    
-    model2 = XGBClassifier()
-    model2.set_params(early_stopping_rounds = 10, **parameters)
-    model2.fit(new_x_train,y_train,
-          eval_set=[(new_x_train,y_train), (new_x_test,y_test)],
-          verbose=0
-          )
-    new_result = model2.score(new_x_test,y_test)
-    print(f"{i}개 컬럼이 삭제되었을 때 Score: ",new_result)
-    result_dict[i] = new_result - results    # 그대로 보면 숫자가 비슷해서 구분하기 힘들기에 얼마나 변했는지 체크
-    
-    
-print(result_dict)
 
 # 최종점수 :  0.7484058070789911
 # acc:  0.7484058070789911
 
 #  5: 0.0002667745238935648
+
+print("="*50)
+thresholds = np.sort(model.feature_importances_)
+# print(thresholds)
+from sklearn.feature_selection import SelectFromModel
+
+for i in thresholds:
+    selection = SelectFromModel(model, threshold=i, prefit=False)
+    
+    select_x_train = selection.transform(x_train)
+    select_x_test = selection.transform(x_test)
+    # print(i, "\t변형된 x_train :", select_x_train.shape, "변형된 x_test :", select_x_test.shape)
+    # i 에는 thresholds 밑에 숫자가 하나씩 나오는데 그거 이상의 숫자를 가진 컬럼은 다 살아남고 / 그거 미만인 컬럼은 사라지는 구조
+    select_model = XGBClassifier()
+    select_model.set_params(
+        early_stopping_rounds=10, **parameters,
+    )
+    
+    select_model.fit(select_x_train, y_train,
+                     eval_set=[(select_x_train, y_train), (select_x_test, y_test)],
+                     verbose=0,)
+    select_y_predict = select_model.predict(select_x_test)
+    score = accuracy_score(y_test, select_y_predict)
+    print("Trech=%.3f, n=%d, ACC %.2f%%" % (i,select_x_train.shape[1], score*100))
+
