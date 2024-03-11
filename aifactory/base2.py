3d85d672219aaf72cfa18a6cbf0ed420821af1a0
lmhjin2 <155138597+lmhjin2@users.noreply.github.com>
2024-03-11 17:53:12 +0900

    2024-03-11 17:53

--- aifactory/base2.py
+++ aifactory/base2.py
@@ -33,9 +33,9 @@ import joblib
 
 ## 사용할 함수 정의
 """
-np.random.seed(33) 
-random.seed(42)           
-tf.random.set_seed(7)
+np.random.seed(99)       # 0
+random.seed(1)         # 42 
+tf.random.set_seed(19)   # 7
 
 MAX_PIXEL_VALUE = 65535 # 이미지 정규화를 위한 픽셀 최대값
 
