f239f5fa43e9929ce8134806ed1100efb5b1b8cf
lmhjin2 <155138597+lmhjin2@users.noreply.github.com>
2024-03-11 12:41:09 +0900

    2024-03-11 12:41

--- aifactory/test1.py
+++ aifactory/test1.py
@@ -38,9 +38,68 @@ tf.random.set_seed(7)
 
 from keras.applications import InceptionV3
 
-inceptionv3 = InceptionV3(include_top=False, weights='mscoco')
-# Load model directly
-from transformers import AutoImageProcessor, SegformerForSemanticSegmentation
+# inceptionv3 = InceptionV3(include_top=False, weights='imagenet')
 
-processor = AutoImageProcessor.from_pretrained("badmatr11x/semantic-image-segmentation")
-model = SegformerForSemanticSegmentation.from_pretrained("badmatr11x/semantic-image-segmentation")
+# Load model directly
+# from transformers import SegformerFeatureExtractor, TFSegformerForSemanticSegmentation
+# from PIL import Image
+# feature_extractor = SegformerFeatureExtractor()
+# model = TFSegformerForSemanticSegmentation()
+
+# model.summary()
+###############################################################################################################
+
+# from transformers import TFSegformerForSemanticSegmentation, SegformerConfig
+
+# # 모델 구성 설정
+# config = SegformerConfig.from_pretrained('google/segformer-b0')
+
+# # 모델 초기화
+# model = TFSegformerForSemanticSegmentation(config)
+
+# model.summary()
+
+###############################################################################################################
+
+
+
+'''###############################################################################################################
+from keras_cv_attention_models import swin_transformer_v2
+
+# Will download and load pretrained imagenet weights.
+mm = swin_transformer_v2.SwinTransformerV2Tiny_window8(pretrained="imagenet")
+
+# Run prediction
+import tensorflow as tf
+from tensorflow import keras
+from skimage.data import chelsea
+imm = keras.applications.imagenet_utils.preprocess_input(chelsea(), mode='torch') # Chelsea the cat
+pred = mm(tf.expand_dims(tf.image.resize(imm, mm.input_shape[1:3]), 0)).numpy()
+print(keras.applications.imagenet_utils.decode_predictions(pred)[0])
+# [('n02124075', 'Egyptian_cat', 0.77100605), ('n02123159', 'tiger_cat', 0.04094378), ...]
+'''
+
+###############################################################################################################
+
+from transformers import MaskFormerImageProcessor, MaskFormerForInstanceSegmentation
+from PIL import Image
+import requests
+
+# load MaskFormer fine-tuned on COCO panoptic segmentation
+processor = MaskFormerImageProcessor.from_pretrained("facebook/maskformer-swin-large-coco")
+model = MaskFormerForInstanceSegmentation.from_pretrained("facebook/maskformer-swin-large-coco")
+
+url = "http://images.cocodataset.org/val2017/000000039769.jpg"
+image = Image.open(requests.get(url, stream=True).raw)
+inputs = processor(images=image, return_tensors="pt")
+
+outputs = model(**inputs)
+# model predicts class_queries_logits of shape `(batch_size, num_queries)`
+# and masks_queries_logits of shape `(batch_size, num_queries, height, width)`
+class_queries_logits = outputs.class_queries_logits
+masks_queries_logits = outputs.masks_queries_logits
+
+# you can pass them to processor for postprocessing
+result = processor.post_process_panoptic_segmentation(outputs, target_sizes=[image.size[::-1]])[0]
+# we refer to the demo notebooks for visualization (see "Resources" section in the MaskFormer docs)
+predicted_panoptic_map = result["segmentation"]
