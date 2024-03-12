86329dd8401251dc37b5b10880d063e6e12a7ab2
lmhjin2 <lmhjin2@naver.com>
2024-03-12 14:25:01 +0900

    2024-03-12 14:24

--- aifactory/attentionUnet.py
+++ aifactory/attentionUnet.py
@@ -1,3 +1,4 @@
+from keras_self_attention import SeqSelfAttention
 import os
 import warnings
 warnings.filterwarnings("ignore")
@@ -8,7 +9,7 @@ from sklearn.model_selection import train_test_split
 import tensorflow as tf
 import keras
 from keras.optimizers import *
-from keras.callbacks import ModelCheckpoint, EarlyStopping
+from keras.callbacks import ModelCheckpoint, EarlyStopping, ReduceLROnPlateau
 from tensorflow.python.keras import backend as K
 import sys
 import pandas as pd
@@ -21,6 +22,9 @@ import os
 import numpy as np
 import sys
 from sklearn.utils import shuffle as shuffle_lists
+from keras.models import Model
+from keras.layers import Input, Conv2D, BatchNormalization, Activation, UpSampling2D, Concatenate, DepthwiseConv2D
+from keras.applications import MobileNetV2
 from keras.models import *
 from keras.layers import *
 import numpy as np
@@ -28,13 +32,10 @@ from keras import backend as K
 from sklearn.model_selection import train_test_split
 import joblib
 
-###################### 전처리 #################################################################################
-
-np.random.seed(0) 
-random.seed(42)           
-tf.random.set_seed(7)
-
+tf.random.set_seed(3)
+np.random.seed(3)
 MAX_PIXEL_VALUE = 65535 # 이미지 정규화를 위한 픽셀 최대값
+THESHOLDS = 0.25
 
 class threadsafe_iter:
     """
@@ -51,6 +52,7 @@ class threadsafe_iter:
         with self.lock:
             return self.it.__next__()
 
+
 def threadsafe_generator(f):
     def g(*a, **kw):
         return threadsafe_iter(f(*a, **kw))
@@ -58,25 +60,38 @@ def threadsafe_generator(f):
     return g
 
 def get_img_arr(path):
-    img = rasterio.open(path).read().transpose((1, 2, 0))
+    img = rasterio.open(path).read().transpose((1, 2, 0))    
     img = np.float32(img)/MAX_PIXEL_VALUE
-
+    
     return img
 
 def get_img_762bands(path):
-    img = rasterio.open(path).read((7,6,2)).transpose((1, 2, 0))
+    img = rasterio.open(path).read((7,6,2)).transpose((1, 2, 0))    
     img = np.float32(img)/MAX_PIXEL_VALUE
-
+    
     return img
-
+    
 def get_mask_arr(path):
     img = rasterio.open(path).read().transpose((1, 2, 0))
     seg = np.float32(img)
     return seg
 
+#miou metric
+def miou(y_true, y_pred, smooth=1e-6):
+    # 임계치 기준으로 이진화
+    y_pred = tf.cast(y_pred > THESHOLDS, tf.float32)
+    
+    intersection = tf.reduce_sum(y_true * y_pred, axis=[1, 2, 3])
+    union = tf.reduce_sum(y_true, axis=[1, 2, 3]) + tf.reduce_sum(y_pred, axis=[1, 2, 3]) - intersection
+    
+    # mIoU 계산
+    iou = (intersection + smooth) / (union + smooth)
+    miou = tf.reduce_mean(iou)
+    return miou
+
 @threadsafe_generator
 def generator_from_lists(images_path, masks_path, batch_size=32, shuffle = True, random_state=None, image_mode='10bands'):
-
+   
     images = []
     masks = []
 
@@ -86,19 +101,20 @@ def generator_from_lists(images_path, masks_path, batch_size=32, shuffle = True,
     if image_mode == '762':
         fopen_image = get_img_762bands
 
-    i = 0
+    i = 0 
     # 데이터 shuffle
     while True:
-
+        
         if shuffle:
             if random_state is None:
                 images_path, masks_path = shuffle_lists(images_path, masks_path)
             else:
                 images_path, masks_path = shuffle_lists(images_path, masks_path, random_state= random_state + i)
-                i += 1
+                i += 1 
+
 
         for img_path, mask_path in zip(images_path, masks_path):
-
+            
             img = fopen_image(img_path)
             mask = fopen_mask(mask_path)
             images.append(img)
@@ -109,52 +125,68 @@ def generator_from_lists(images_path, masks_path, batch_size=32, shuffle = True,
                 images = []
                 masks = []
 
-###################### 전처리 #################################################################################
-BATCH_SIZE = 8 # batch size 지정
 
-######################  STV2  #################################################################################
-import math
-import numpy as np
-from keras_cv_attention_models import backend
-from keras_cv_attention_models.backend import layers, functional, models, initializers
-from keras_cv_attention_models.models import register_model
-from keras_cv_attention_models.attention_layers import (
-    BiasLayer,
-    drop_block,
-    layer_norm,
-    mlp_block,
-    output_block,
-    add_pre_post_process,
-)
-from keras_cv_attention_models.download_and_load import reload_model_weights
-PRETRAINED_DICT = {
-    "swin_transformer_v2_tiny_window8": {"imagenet": {256: "97ece5f8d8012d6d40797df063a5f02b"}}
-}
-# from keras_cv_attention_models import swin_transformer_v2
-# base_model = swin_transformer_v2.SwinTransformerV2Tiny_window8(input_shape=(256,256,3),
-#                                                                num_classes=1,
-#                                                                pretrained="imagenet", 
-#                                                        classifier_activation="sigmoid",
-#                                                        )
-from timm import create_model
+###################################################################
+def conv_block(input_tensor, num_filters):
+    encoder = Conv2D(num_filters, (3, 3), padding='same')(input_tensor)
+    encoder = BatchNormalization()(encoder)
+    encoder = Activation('relu')(encoder)
+    encoder = Conv2D(num_filters, (3, 3), padding='same')(encoder)
+    encoder = BatchNormalization()(encoder)
+    encoder = Activation('relu')(encoder)
+    return encoder
 
-base_model = create_model(
-    "swinv2_tiny_patch4_window8_256",
-    img_size=(256, 256),
-    patch_size=4,
-    embed_dim=96,
-    depths=(2, 2, 6, 2),
-    num_heads=(3, 6, 12, 24),
-    drop_path_rate=0.1,
-)
-# x = base_model.output
-# x = Dense(1, activation='sigmoid')(x)
-# model = Model(inputs=base_model.input, outputs=x)
-model = base_model
-model.summary()
-######################  STV2  #################################################################################
+def attention_block(input_tensor, num_filters):
+    att = Conv2D(num_filters, (1, 1), padding='same')(input_tensor)
+    att = BatchNormalization()(att)
+    att = Activation('relu')(att)
+    return att
 
-###################### 전처리 #################################################################################
+def up_block(input_tensor, concat_tensor, num_filters):
+    up = UpSampling2D((2, 2))(input_tensor)
+    up = Concatenate()([up, concat_tensor])
+    up = conv_block(up, num_filters)
+    return up
+
+def get_attention_unet(input_height=256, input_width=256, n_channels=3, n_classes=1, num_filters=16):
+    inputs = Input((input_height, input_width, n_channels))
+
+    # Encoder
+    enc1 = conv_block(inputs, num_filters)
+    pool1 = MaxPooling2D(pool_size=(2, 2))(enc1)
+
+    enc2 = conv_block(pool1, num_filters*2)
+    pool2 = MaxPooling2D(pool_size=(2, 2))(enc2)
+
+    enc3 = conv_block(pool2, num_filters*4)
+    pool3 = MaxPooling2D(pool_size=(2, 2))(enc3)
+
+    enc4 = conv_block(pool3, num_filters*8)
+    pool4 = MaxPooling2D(pool_size=(2, 2))(enc4)
+
+    # Center
+    center = conv_block(pool4, num_filters*16)
+
+    # Decoder
+    att4 = attention_block(center, num_filters*8)
+    dec4 = up_block(att4, enc4, num_filters*8)
+
+    att3 = attention_block(dec4, num_filters*4)
+    dec3 = up_block(att3, enc3, num_filters*4)
+
+    att2 = attention_block(dec3, num_filters*2)
+    dec2 = up_block(att2, enc2, num_filters*2)
+
+    att1 = attention_block(dec2, num_filters)
+    dec1 = up_block(att1, enc1, num_filters)
+
+    outputs = Conv2D(n_classes, (1, 1), activation='sigmoid')(dec1)
+
+    model = Model(inputs=[inputs], outputs=[outputs])
+    return model
+
+
+###################################################################
 
 # 두 샘플 간의 유사성 metric
 def dice_coef(y_true, y_pred, smooth=1):
@@ -167,27 +199,29 @@ def dice_coef(y_true, y_pred, smooth=1):
 def pixel_accuracy (y_true, y_pred):
     sum_n = np.sum(np.logical_and(y_pred, y_true))
     sum_t = np.sum(y_true)
-
+ 
     if (sum_t == 0):
         pixel_accuracy = 0
     else:
         pixel_accuracy = sum_n / sum_t
-    return pixel_accuracy
+    return pixel_accuracy                   
+
 
 # 사용할 데이터의 meta정보 가져오기
+
 train_meta = pd.read_csv('d:/data/aispark/dataset/train_meta.csv')
 test_meta = pd.read_csv('d:/data/aispark/dataset/test_meta.csv')
 
-# 저장 이름
-save_name = 'STV2'
+#  저장 이름
+save_name = 'attention_unet2'
 
 N_FILTERS = 16 # 필터수 지정
 N_CHANNELS = 3 # channel 지정
-EPOCHS = 10 # 훈련 epoch 지정
-BATCH_SIZE = BATCH_SIZE # batch size 지정
+EPOCHS = 50 # 훈련 epoch 지정
+BATCH_SIZE = 20  # batch size 지정
 IMAGE_SIZE = (256, 256) # 이미지 크기 지정
-MODEL_NAME = 'STV2' # 모델 이름
-RANDOM_STATE = 47 # seed 고정
+MODEL_NAME = 'attention' # 모델 이름
+RANDOM_STATE = 3144 # seed 고정
 INITIAL_EPOCH = 0 # 초기 epoch
 
 # 데이터 위치
@@ -199,14 +233,14 @@ OUTPUT_DIR = 'c:/Study/aifactory/train_output/'
 WORKERS = 8    # 원래 4 // (코어 / 2 ~ 코어) 
 
 # 조기종료
-EARLY_STOP_PATIENCE = 5
+EARLY_STOP_PATIENCE = 20
 
 # 중간 가중치 저장 이름
 CHECKPOINT_PERIOD = 5
-CHECKPOINT_MODEL_NAME = 'checkpoint-{}-{}-epoch_{{epoch:02d}}.hdf5'.format(MODEL_NAME, save_name)
-
+CHECKPOINT_MODEL_NAME = 'checkpoint-{}-{}-epoch_{{epoch:02d}}_attention2.hdf5'.format(MODEL_NAME, save_name)
+ 
 # 최종 가중치 저장 이름
-FINAL_WEIGHTS_OUTPUT = 'model_{}_{}_final_weights.h5'.format(MODEL_NAME, save_name)
+FINAL_WEIGHTS_OUTPUT = 'model_{}_{}_attention2.h5'.format(MODEL_NAME, save_name)
 
 # 사용할 GPU 이름
 CUDA_DEVICE = 0
@@ -247,28 +281,18 @@ masks_validation = [os.path.join(MASKS_PATH, mask) for mask in x_val['train_mask
 train_generator = generator_from_lists(images_train, masks_train, batch_size=BATCH_SIZE, random_state=RANDOM_STATE, image_mode="762")
 validation_generator = generator_from_lists(images_validation, masks_validation, batch_size=BATCH_SIZE, random_state=RANDOM_STATE, image_mode="762")
 
-from sklearn.metrics import f1_score
-def my_f1(y_true,y_pred):
-    score = tf.py_function(func=f1_score, inp=[y_true,y_pred], Tout=tf.float32, name='f1_score')
-    return score
+model = get_attention_unet()
 
-###################### 전처리 #################################################################################
-
-
-######################  훈련  #################################################################################
-
-# model 불러오기
-# model = get_model(MODEL_NAME, input_height=IMAGE_SIZE[0], input_width=IMAGE_SIZE[1], n_filters=N_FILTERS, n_channels=N_CHANNELS)
-# model.compile(optimizer = Adam(), loss = 'binary_crossentropy', metrics = ['acc'])
-model.compile(optimizer = Adam(), loss = 'binary_crossentropy', metrics = ['acc'])
+learning_rate = 0.001
+model.compile(optimizer=Adam(learning_rate=learning_rate), loss='binary_crossentropy', metrics=['accuracy', miou])
 model.summary()
 
 # checkpoint 및 조기종료 설정
-es = EarlyStopping(monitor='val_loss', mode='min', verbose=1, patience=EARLY_STOP_PATIENCE,restore_best_weights=True)
+es = EarlyStopping(monitor='val_loss', mode='min', verbose=1, patience=EARLY_STOP_PATIENCE, restore_best_weights=True)
 checkpoint = ModelCheckpoint(os.path.join(OUTPUT_DIR, CHECKPOINT_MODEL_NAME), monitor='val_loss', verbose=1,
 save_best_only=True, mode='auto', period=CHECKPOINT_PERIOD)
-
-
+# Reduce
+rlr = ReduceLROnPlateau(monitor='val_loss',factor=0.5, patience=10, verbose=1, mode='auto')
 
 print('---model 훈련 시작---')
 history = model.fit_generator(
@@ -276,30 +300,19 @@ history = model.fit_generator(
     steps_per_epoch=len(images_train) // BATCH_SIZE,
     validation_data=validation_generator,
     validation_steps=len(images_validation) // BATCH_SIZE,
-    callbacks=[checkpoint, es],
+    callbacks=[checkpoint, es, rlr],
     epochs=EPOCHS,
     workers=WORKERS,
     initial_epoch=INITIAL_EPOCH
 )
 print('---model 훈련 종료---')
 
-## model save
 print('가중치 저장')
 model_weights_output = os.path.join(OUTPUT_DIR, FINAL_WEIGHTS_OUTPUT)
 model.save_weights(model_weights_output)
 print("저장된 가중치 명: {}".format(model_weights_output))
 
-######################  훈련  #################################################################################
-
-# model = get_model(MODEL_NAME, input_height=IMAGE_SIZE[0], input_width=IMAGE_SIZE[1], n_filters=N_FILTERS, n_channels=N_CHANNELS)
-model.compile(optimizer = Adam(), loss = 'binary_crossentropy', metrics = ['accuracy'])
-model.summary()
-
-model.load_weights('c:/Study/aifactory/train_output/STV2_final_weights.h5')
-
-"""## 제출 Predict
-- numpy astype uint8로 지정
-- 반드시 pkl로 저장"""
+model.load_weights('c:/Study/aifactory/train_output/model_unet_base_line_final_weights.h5')
 
 y_pred_dict = {}
 
@@ -307,12 +320,11 @@ for i in test_meta['test_img']:
     img = get_img_762bands(f'd:/data/aispark/dataset/test_img/{i}')
     y_pred = model.predict(np.array([img]), batch_size=1)
 
-    y_pred = np.where(y_pred[0, :, :, 0] > 0.5, 1, 0) # 임계값 처리
+    y_pred = np.where(y_pred[0, :, :, 0] > 0.25, 1, 0) # 임계값 처리
     y_pred = y_pred.astype(np.uint8)
     y_pred_dict[i] = y_pred
 
 from datetime import datetime
 dt = datetime.now()
 joblib.dump(y_pred_dict, f'c:/Study/aifactory/train_output/y_pred_{dt.day}_{dt.hour}_{dt.minute}.pkl')
-
 
--- aifactory/swin_transformer_v2_3.py
+++ aifactory/swin_transformer_v2_3.py
@@ -27,12 +27,12 @@ import numpy as np
 from keras import backend as K
 from sklearn.model_selection import train_test_split
 import joblib
+from keras_cv_attention_models import swin_transformer_v2
+from transformers import Swinv2Backbone, Swinv2Config, Swinv2Model, Swinv2PreTrainedModel
 
-###################### 전처리 #################################################################################
-
-np.random.seed(0) 
-random.seed(42)           
-tf.random.set_seed(7)
+np.random.seed(9)       # 0
+random.seed(1)         # 42
+tf.random.set_seed(99)   # 7
 
 MAX_PIXEL_VALUE = 65535 # 이미지 정규화를 위한 픽셀 최대값
 
@@ -50,6 +50,7 @@ class threadsafe_iter:
     def __next__(self):
         with self.lock:
             return self.it.__next__()
+
 
 def threadsafe_generator(f):
     def g(*a, **kw):
@@ -74,6 +75,8 @@ def get_mask_arr(path):
     seg = np.float32(img)
     return seg
 
+
+
 @threadsafe_generator
 def generator_from_lists(images_path, masks_path, batch_size=32, shuffle = True, random_state=None, image_mode='10bands'):
 
@@ -97,6 +100,7 @@ def generator_from_lists(images_path, masks_path, batch_size=32, shuffle = True,
                 images_path, masks_path = shuffle_lists(images_path, masks_path, random_state= random_state + i)
                 i += 1
 
+
         for img_path, mask_path in zip(images_path, masks_path):
 
             img = fopen_image(img_path)
@@ -109,52 +113,168 @@ def generator_from_lists(images_path, masks_path, batch_size=32, shuffle = True,
                 images = []
                 masks = []
 
-###################### 전처리 #################################################################################
-BATCH_SIZE = 8 # batch size 지정
+# Unet 모델 정의
+def FCN(nClasses, input_height=128, input_width=128, n_filters = 16, dropout = 0.1, batchnorm = True):
 
-######################  STV2  #################################################################################
-import math
-import numpy as np
-from keras_cv_attention_models import backend
-from keras_cv_attention_models.backend import layers, functional, models, initializers
-from keras_cv_attention_models.models import register_model
-from keras_cv_attention_models.attention_layers import (
-    BiasLayer,
-    drop_block,
-    layer_norm,
-    mlp_block,
-    output_block,
-    add_pre_post_process,
-)
-from keras_cv_attention_models.download_and_load import reload_model_weights
-PRETRAINED_DICT = {
-    "swin_transformer_v2_tiny_window8": {"imagenet": {256: "97ece5f8d8012d6d40797df063a5f02b"}}
-}
-# from keras_cv_attention_models import swin_transformer_v2
-# base_model = swin_transformer_v2.SwinTransformerV2Tiny_window8(input_shape=(256,256,3),
-#                                                                num_classes=1,
-#                                                                pretrained="imagenet", 
-#                                                        classifier_activation="sigmoid",
-#                                                        )
-from timm import create_model
 
-base_model = create_model(
-    "swinv2_tiny_patch4_window8_256",
-    img_size=(256, 256),
-    patch_size=4,
-    embed_dim=96,
-    depths=(2, 2, 6, 2),
-    num_heads=(3, 6, 12, 24),
-    drop_path_rate=0.1,
-)
-# x = base_model.output
-# x = Dense(1, activation='sigmoid')(x)
-# model = Model(inputs=base_model.input, outputs=x)
-model = base_model
-model.summary()
-######################  STV2  #################################################################################
+    img_input = Input(shape=(input_height,input_width, 3))
 
-###################### 전처리 #################################################################################
+    ## Block 1
+    x = Conv2D(n_filters, (3, 3), activation='relu', padding='same', name='block1_conv1')(img_input)
+    x = Conv2D(n_filters, (3, 3), activation='relu', padding='same', name='block1_conv2')(x)
+    f1 = x
+
+    # Block 2
+    x = Conv2D(n_filters, (3, 3), activation='relu', padding='same', name='block2_conv1')(x)
+    x = Conv2D(n_filters, (3, 3), activation='relu', padding='same', name='block2_conv2')(x)
+    f2 = x
+
+    # Out
+    o = (Conv2D(nClasses, (3,3), activation='relu' , padding='same', name="Out"))(x)
+
+    model = Model(img_input, o)
+
+    return model
+
+
+def conv2d_block(input_tensor, n_filters, kernel_size = 3, batchnorm = True):
+    # first layer
+    x = Conv2D(filters=n_filters, kernel_size=(kernel_size, kernel_size), kernel_initializer="he_normal",
+               padding="same")(input_tensor)
+    if batchnorm:
+        x = BatchNormalization()(x)
+    x = Activation("relu")(x)
+
+    # second layer
+    x = Conv2D(filters=n_filters, kernel_size=(kernel_size, kernel_size), kernel_initializer="he_normal",
+               padding="same")(x)
+    if batchnorm:
+        x = BatchNormalization()(x)
+    x = Activation("relu")(x)
+    return x
+
+def get_unet(nClasses, input_height=256, input_width=256, n_filters = 16, dropout = 0.1, batchnorm = True, n_channels=10):
+    input_img = Input(shape=(input_height,input_width, n_channels))
+
+    # contracting path
+    c1 = conv2d_block(input_img, n_filters=n_filters*1, kernel_size=3, batchnorm=batchnorm)
+    p1 = MaxPooling2D((2, 2)) (c1)
+    p1 = Dropout(dropout)(p1)
+
+    c2 = conv2d_block(p1, n_filters=n_filters*2, kernel_size=3, batchnorm=batchnorm)
+    p2 = MaxPooling2D((2, 2)) (c2)
+    p2 = Dropout(dropout)(p2)
+
+    c3 = conv2d_block(p2, n_filters=n_filters*4, kernel_size=3, batchnorm=batchnorm)
+    p3 = MaxPooling2D((2, 2)) (c3)
+    p3 = Dropout(dropout)(p3)
+
+    c4 = conv2d_block(p3, n_filters=n_filters*8, kernel_size=3, batchnorm=batchnorm)
+    p4 = MaxPooling2D(pool_size=(2, 2)) (c4)
+    p4 = Dropout(dropout)(p4)
+
+    c5 = conv2d_block(p4, n_filters=n_filters*16, kernel_size=3, batchnorm=batchnorm)
+
+    # expansive path
+    u6 = Conv2DTranspose(n_filters*8, (3, 3), strides=(2, 2), padding='same') (c5)
+    u6 = concatenate([u6, c4])
+    u6 = Dropout(dropout)(u6)
+    c6 = conv2d_block(u6, n_filters=n_filters*8, kernel_size=3, batchnorm=batchnorm)
+
+    u7 = Conv2DTranspose(n_filters*4, (3, 3), strides=(2, 2), padding='same') (c6)
+    u7 = concatenate([u7, c3])
+    u7 = Dropout(dropout)(u7)
+    c7 = conv2d_block(u7, n_filters=n_filters*4, kernel_size=3, batchnorm=batchnorm)
+
+    u8 = Conv2DTranspose(n_filters*2, (3, 3), strides=(2, 2), padding='same') (c7)
+    u8 = concatenate([u8, c2])
+    u8 = Dropout(dropout)(u8)
+    c8 = conv2d_block(u8, n_filters=n_filters*2, kernel_size=3, batchnorm=batchnorm)
+
+    u9 = Conv2DTranspose(n_filters*1, (3, 3), strides=(2, 2), padding='same') (c8)
+    u9 = concatenate([u9, c1], axis=3)
+    u9 = Dropout(dropout)(u9)
+    c9 = conv2d_block(u9, n_filters=n_filters*1, kernel_size=3, batchnorm=batchnorm)
+
+    outputs = Conv2D(1, (1, 1), activation='sigmoid') (c9)
+    model = Model(inputs=[input_img], outputs=[outputs])
+    return model
+
+
+
+def get_unet_small1 (nClasses, input_height=128, input_width=128, n_filters = 16, dropout = 0.1, batchnorm = True, n_channels=3):
+
+    input_img = Input(shape=(input_height,input_width, n_channels))
+
+    # Contracting Path
+    c1 = conv2d_block(input_img, n_filters * 1, kernel_size = 3, batchnorm = batchnorm)
+    p1 = MaxPooling2D((2, 2))(c1)
+    p1 = Dropout(dropout)(p1)
+
+    c2 = conv2d_block(p1, n_filters * 1, kernel_size = 3, batchnorm = batchnorm)
+    p2 = MaxPooling2D((2, 2))(c2)
+    p2 = Dropout(dropout)(p2)
+
+    c3 = conv2d_block(p2, n_filters = n_filters * 2, kernel_size = 3, batchnorm = batchnorm)
+
+    # Expansive Path
+    u8 = Conv2DTranspose(n_filters * 2, (3, 3), strides = (2, 2), padding = 'same')(c3)
+    u8 = concatenate([u8, c2])
+    u8 = Dropout(dropout)(u8)
+    c8 = conv2d_block(u8, n_filters * 1, kernel_size = 3, batchnorm = batchnorm)
+
+    u9 = Conv2DTranspose(n_filters * 1, (3, 3), strides = (2, 2), padding = 'same')(c8)
+    u9 = concatenate([u9, c1])
+    u9 = Dropout(dropout)(u9)
+    c9 = conv2d_block(u9, n_filters * 1, kernel_size = 3, batchnorm = batchnorm)
+
+    outputs = Conv2D(1, (1, 1), activation='relu')(c9)
+    model = Model(inputs=[input_img], outputs=[outputs])
+    return model
+
+
+
+
+def get_unet_small2 (nClasses, input_height=128, input_width=128, n_filters = 16, dropout = 0.1, batchnorm = True, n_channels=3):
+
+    input_img = Input(shape=(input_height,input_width, n_channels))
+
+    # Contracting Path
+    c1 = conv2d_block(input_img, n_filters * 1, kernel_size = 3, batchnorm = batchnorm)
+    p1 = MaxPooling2D((2, 2))(c1)
+    p1 = Dropout(dropout)(p1)
+
+    c2 = conv2d_block(p1, n_filters = n_filters * 4, kernel_size = 3, batchnorm = batchnorm)
+
+    # Expansive Path
+    u3 = Conv2DTranspose(n_filters * 1, (3, 3), strides = (2, 2), padding = 'same')(c2)
+    u3 = concatenate([u3, c1])
+    u3 = Dropout(dropout)(u3)
+    c3 = conv2d_block(u3, n_filters * 1, kernel_size = 3, batchnorm = batchnorm)
+
+    outputs = Conv2D(1, (1, 1), activation='relu')(c3)
+    model = Model(inputs=[input_img], outputs=[outputs])
+    return model
+
+def get_model(model_name, nClasses=1, input_height=128, input_width=128, n_filters = 16, dropout = 0.1, batchnorm = True, n_channels=10):
+    if model_name == 'fcn':
+        model = FCN
+    elif model_name == 'unet':
+        model = get_unet
+    elif model_name == 'unet_small':
+        model = get_unet_small1
+    elif model_name == 'unet_smaller':
+        model = get_unet_small2
+
+    return model(
+            nClasses      = nClasses,
+            input_height  = input_height,
+            input_width   = input_width,
+            n_filters     = n_filters,
+            dropout       = dropout,
+            batchnorm     = batchnorm,
+            n_channels    = n_channels
+        )
 
 # 두 샘플 간의 유사성 metric
 def dice_coef(y_true, y_pred, smooth=1):
@@ -174,19 +294,26 @@ def pixel_accuracy (y_true, y_pred):
         pixel_accuracy = sum_n / sum_t
     return pixel_accuracy
 
+"""&nbsp;
+
+## parameter 설정
+"""
+
 # 사용할 데이터의 meta정보 가져오기
+
 train_meta = pd.read_csv('d:/data/aispark/dataset/train_meta.csv')
 test_meta = pd.read_csv('d:/data/aispark/dataset/test_meta.csv')
 
+
 # 저장 이름
-save_name = 'STV2'
+save_name = 'base_line'
 
 N_FILTERS = 16 # 필터수 지정
 N_CHANNELS = 3 # channel 지정
-EPOCHS = 10 # 훈련 epoch 지정
-BATCH_SIZE = BATCH_SIZE # batch size 지정
+EPOCHS = 50 # 훈련 epoch 지정
+BATCH_SIZE = 8 # batch size 지정
 IMAGE_SIZE = (256, 256) # 이미지 크기 지정
-MODEL_NAME = 'STV2' # 모델 이름
+MODEL_NAME = 'unet' # 모델 이름
 RANDOM_STATE = 47 # seed 고정
 INITIAL_EPOCH = 0 # 초기 epoch
 
@@ -252,23 +379,24 @@ def my_f1(y_true,y_pred):
     score = tf.py_function(func=f1_score, inp=[y_true,y_pred], Tout=tf.float32, name='f1_score')
     return score
 
-###################### 전처리 #################################################################################
-
-
-######################  훈련  #################################################################################
-
 # model 불러오기
-# model = get_model(MODEL_NAME, input_height=IMAGE_SIZE[0], input_width=IMAGE_SIZE[1], n_filters=N_FILTERS, n_channels=N_CHANNELS)
-# model.compile(optimizer = Adam(), loss = 'binary_crossentropy', metrics = ['acc'])
+model = get_model(MODEL_NAME, input_height=IMAGE_SIZE[0], input_width=IMAGE_SIZE[1], n_filters=N_FILTERS, n_channels=N_CHANNELS)
 model.compile(optimizer = Adam(), loss = 'binary_crossentropy', metrics = ['acc'])
 model.summary()
 
+print(np.unique(x_tr.shape,return_counts=True))
+print(np.unique(x_val.shape,return_counts=True))
+
+
 # checkpoint 및 조기종료 설정
 es = EarlyStopping(monitor='val_loss', mode='min', verbose=1, patience=EARLY_STOP_PATIENCE,restore_best_weights=True)
-checkpoint = ModelCheckpoint(os.path.join(OUTPUT_DIR, CHECKPOINT_MODEL_NAME), monitor='val_loss', verbose=1,
+checkpoint = ModelCheckpoint(os.path.join(OUTPUT_DIR, CHECKPOINT_MODEL_NAME), monitor='loss', verbose=1,
 save_best_only=True, mode='auto', period=CHECKPOINT_PERIOD)
 
+"""&nbsp;
 
+## model 훈련
+"""
 
 print('---model 훈련 시작---')
 history = model.fit_generator(
@@ -283,23 +411,32 @@ history = model.fit_generator(
 )
 print('---model 훈련 종료---')
 
+"""&nbsp;
+
 ## model save
+"""
+
 print('가중치 저장')
 model_weights_output = os.path.join(OUTPUT_DIR, FINAL_WEIGHTS_OUTPUT)
 model.save_weights(model_weights_output)
 print("저장된 가중치 명: {}".format(model_weights_output))
 
-######################  훈련  #################################################################################
+"""## inference
 
-# model = get_model(MODEL_NAME, input_height=IMAGE_SIZE[0], input_width=IMAGE_SIZE[1], n_filters=N_FILTERS, n_channels=N_CHANNELS)
+- 학습한 모델 불러오기
+"""
+
+model = get_model(MODEL_NAME, input_height=IMAGE_SIZE[0], input_width=IMAGE_SIZE[1], n_filters=N_FILTERS, n_channels=N_CHANNELS)
 model.compile(optimizer = Adam(), loss = 'binary_crossentropy', metrics = ['accuracy'])
 model.summary()
 
-model.load_weights('c:/Study/aifactory/train_output/STV2_final_weights.h5')
+model.load_weights('c:/Study/aifactory/train_output/model_unet_base_line_final_weights.h5')
 
 """## 제출 Predict
 - numpy astype uint8로 지정
-- 반드시 pkl로 저장"""
+- 반드시 pkl로 저장
+
+"""
 
 y_pred_dict = {}
 
@@ -314,5 +451,4 @@ for i in test_meta['test_img']:
 from datetime import datetime
 dt = datetime.now()
 joblib.dump(y_pred_dict, f'c:/Study/aifactory/train_output/y_pred_{dt.day}_{dt.hour}_{dt.minute}.pkl')
-
 
