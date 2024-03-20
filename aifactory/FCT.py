b37cf0c322f2dd14e69026feed3fa49a7e33a7ae
lmhjin2 <lmhjin2@naver.com>
2024-03-20 13:10:56 +0900

    2024-03-20 13:10

---
+++ aifactory/FCT.py
@@ -0,0 +1,294 @@
+import os
+import warnings
+warnings.filterwarnings("ignore")
+import glob
+import matplotlib.pyplot as plt
+from sklearn.metrics import confusion_matrix
+from sklearn.model_selection import train_test_split
+import tensorflow as tf
+import keras
+from keras.optimizers import *
+from keras.callbacks import ModelCheckpoint, EarlyStopping, ReduceLROnPlateau
+from tensorflow.python.keras import backend as K
+import sys
+import pandas as pd
+from tqdm import tqdm
+from keras.preprocessing.image import ImageDataGenerator
+import threading
+import random
+import rasterio
+import os
+import numpy as np
+import sys
+from sklearn.utils import shuffle as shuffle_lists
+from keras.models import *
+from keras.layers import *
+import numpy as np
+from keras import backend as K
+from sklearn.model_selection import train_test_split
+import joblib
+import segmentation_models as sm
+import tensorflow_addons as tfa
+from utils import *
+
+"""&nbsp;
+
+## 사용할 함수 정의
+"""
+np.random.seed(19)       # 0
+random.seed(1)         # 42
+tf.random.set_seed(99)   # 7
+
+MAX_PIXEL_VALUE = 65535 # 이미지 정규화를 위한 픽셀 최대값
+
+class threadsafe_iter:
+    """
+    데이터 불러올떼, 호출 직렬화
+    """
+    def __init__(self, it):
+        self.it = it
+        self.lock = threading.Lock()
+
+    def __iter__(self):
+        return self
+
+    def __next__(self):
+        with self.lock:
+            return self.it.__next__()
+
+
+def threadsafe_generator(f):
+    def g(*a, **kw):
+        return threadsafe_iter(f(*a, **kw))
+
+    return g
+
+def get_img_arr(path):
+    img = rasterio.open(path).read().transpose((1, 2, 0))
+    img = np.float32(img)/MAX_PIXEL_VALUE
+
+    return img
+
+def get_img_762bands(path):
+    img = rasterio.open(path).read((7,6,2)).transpose((1, 2, 0))
+    img = np.float32(img)/MAX_PIXEL_VALUE
+
+    return img
+
+def get_mask_arr(path):
+    img = rasterio.open(path).read().transpose((1, 2, 0))
+    seg = np.float32(img)
+    return seg
+
+
+
+@threadsafe_generator
+def generator_from_lists(images_path, masks_path, batch_size=32, shuffle = True, random_state=None, image_mode='10bands'):
+
+    images = []
+    masks = []
+
+    fopen_image = get_img_arr
+    fopen_mask = get_mask_arr
+
+    if image_mode == '762':
+        fopen_image = get_img_762bands
+
+    i = 0
+    # 데이터 shuffle
+    while True:
+
+        if shuffle:
+            if random_state is None:
+                images_path, masks_path = shuffle_lists(images_path, masks_path)
+            else:
+                images_path, masks_path = shuffle_lists(images_path, masks_path, random_state= random_state + i)
+                i += 1
+
+
+        for img_path, mask_path in zip(images_path, masks_path):
+
+            img = fopen_image(img_path)
+            mask = fopen_mask(mask_path)
+            images.append(img)
+            masks.append(mask)
+
+            if len(images) >= batch_size:
+                yield (np.array(images), np.array(masks))
+                images = []
+                masks = []
+
+# 두 샘플 간의 유사성 metric
+def dice_coef(y_true, y_pred, smooth=1):
+    intersection = K.sum(y_true * y_pred, axis=[1,2,3])
+    union = K.sum(y_true, axis=[1,2,3]) + K.sum(y_pred, axis=[1,2,3])
+    dice = K.mean((2. * intersection + smooth)/(union + smooth), axis=0)
+    return dice
+
+# 픽셀 정확도를 계산 metric
+def pixel_accuracy (y_true, y_pred):
+    sum_n = np.sum(np.logical_and(y_pred, y_true))
+    sum_t = np.sum(y_true)
+
+    if (sum_t == 0):
+        pixel_accuracy = 0
+    else:
+        pixel_accuracy = sum_n / sum_t
+    return pixel_accuracy
+
+"""&nbsp;
+
+## parameter 설정
+"""
+
+# 사용할 데이터의 meta정보 가져오기
+
+train_meta = pd.read_csv('c:/Study/aifactory/dataset/train_meta.csv')
+test_meta = pd.read_csv('c:/Study/aifactory/dataset/test_meta.csv')
+
+
+# 저장 이름
+save_name = 'FCT'
+
+N_FILTERS = 16 # 필터수 지정
+N_CHANNELS = 3 # channel 지정
+EPOCHS = 50 # 훈련 epoch 지정
+BATCH_SIZE = 1 # batch size 지정
+IMAGE_SIZE = (256, 256) # 이미지 크기 지정
+MODEL_NAME = 'FCT' # 모델 이름
+RANDOM_STATE = 42 # seed 고정
+INITIAL_EPOCH = 0 # 초기 epoch
+
+# 데이터 위치
+IMAGES_PATH = 'c:/Study/aifactory/dataset/train_img/'
+MASKS_PATH = 'c:/Study/aifactory/dataset/train_mask/'
+
+# 가중치 저장 위치
+OUTPUT_DIR = 'c:/Study/aifactory/train_output/'
+WORKERS = 16    # 원래 4 // (코어 / 2 ~ 코어) 
+
+# 조기종료
+# EARLY_STOP_PATIENCE = 20
+
+# 중간 가중치 저장 이름
+CHECKPOINT_PERIOD = 5
+CHECKPOINT_MODEL_NAME = 'checkpoint-{}-{}-epoch_{{epoch:02d}}.hdf5'.format(MODEL_NAME, save_name)
+
+# 최종 가중치 저장 이름
+FINAL_WEIGHTS_OUTPUT = 'model_{}_{}_final_weights.h5'.format(MODEL_NAME, save_name)
+
+# 사용할 GPU 이름
+CUDA_DEVICE = 0
+
+
+# 저장 폴더 없으면 생성
+if not os.path.exists(OUTPUT_DIR):
+    os.makedirs(OUTPUT_DIR)
+
+
+# GPU 설정
+os.environ["CUDA_VISIBLE_DEVICES"] = str(CUDA_DEVICE)
+try:
+    config = tf.compat.v1.ConfigProto()
+    config.gpu_options.allow_growth = True
+    sess = tf.compat.v1.Session(config=config)
+    K.set_session(sess)
+except:
+    pass
+
+try:
+    np.random.bit_generator = np.random._bit_generator
+except:
+    pass
+
+
+# train : val = 8 : 2 나누기
+x_tr, x_val = train_test_split(train_meta, test_size=0.2, random_state=RANDOM_STATE)
+# print(len(x_tr), len(x_val))
+
+# train : val 지정 및 generator
+images_train = [os.path.join(IMAGES_PATH, image) for image in x_tr['train_img'] ]
+masks_train = [os.path.join(MASKS_PATH, mask) for mask in x_tr['train_mask'] ]
+
+images_validation = [os.path.join(IMAGES_PATH, image) for image in x_val['train_img'] ]
+masks_validation = [os.path.join(MASKS_PATH, mask) for mask in x_val['train_mask'] ]
+
+train_generator = generator_from_lists(images_train, masks_train, batch_size=BATCH_SIZE, random_state=RANDOM_STATE, image_mode="762")
+validation_generator = generator_from_lists(images_validation, masks_validation, batch_size=BATCH_SIZE, random_state=RANDOM_STATE, image_mode="762")
+
+from sklearn.metrics import f1_score
+def my_f1(y_true,y_pred):
+    score = tf.py_function(func=f1_score, inp=[y_true,y_pred], Tout=tf.float32, name='f1_score')
+    return score
+
+# model 불러오기
+input_shape = (256,256,3)
+model = FCT(input_shape=input_shape)
+model.summary()
+optimizer = tfa.optimizers.AdamW(learning_rate=0.01, weight_decay=1e-4)
+model.compile(optimizer = optimizer,
+            #   loss = sm.losses.bce_jaccard_loss , 
+            #   loss = 'binary_crossentropy',
+              loss = sm.losses.binary_focal_dice_loss, 
+              metrics = ['acc', sm.metrics.iou_score])
+model.summary()
+
+# checkpoint 및 조기종료 설정
+es = EarlyStopping(monitor='val_iou_score', mode='max', verbose=1, patience=10, restore_best_weights=True)
+checkpoint = ModelCheckpoint(os.path.join(OUTPUT_DIR, CHECKPOINT_MODEL_NAME), monitor='val_iou_score', verbose=1,
+save_best_only=True, mode='max', period=CHECKPOINT_PERIOD)
+# rlr
+rlr = ReduceLROnPlateau(monitor='val_iou_score', mode='max', patience=5, verbose=1, factor=0.1)
+
+
+print('---model 훈련 시작---')
+history = model.fit_generator(
+    train_generator,
+    steps_per_epoch=len(images_train) // BATCH_SIZE,
+    validation_data=validation_generator,
+    validation_steps=len(images_validation) // BATCH_SIZE,
+    callbacks=[checkpoint, es, rlr],
+    epochs=50,
+    workers=WORKERS,
+    initial_epoch=INITIAL_EPOCH
+)
+print('---model 훈련 종료---')
+
+"""&nbsp;
+
+## model save
+"""
+
+print('가중치 저장')
+model_weights_output = os.path.join(OUTPUT_DIR, FINAL_WEIGHTS_OUTPUT)
+model.save_weights(model_weights_output)
+print("저장된 가중치 명: {}".format(model_weights_output))
+
+"""## inference
+
+- 학습한 모델 불러오기
+"""
+
+# model.load_weights('c:/Study/aifactory/train_output/model_unet_base_line_final_weights.h5')
+
+"""## 제출 Predict
+- numpy astype uint8로 지정
+- 반드시 pkl로 저장
+
+"""
+
+y_pred_dict = {}
+
+for i in test_meta['test_img']:
+    img = get_img_762bands(f'c:/Study/aifactory/dataset/test_img/{i}')
+    y_pred = model.predict(np.array([img]), batch_size=1)
+
+    y_pred = np.where(y_pred[0, :, :, 0] > 0.25, 1, 0) # 임계값 처리
+    y_pred = y_pred.astype(np.uint8)
+    y_pred_dict[i] = y_pred
+
+from datetime import datetime
+dt = datetime.now()
+joblib.dump(y_pred_dict, f'c:/Study/aifactory/train_output/y_pred_{dt.day}_{dt.hour}_{dt.minute}.pkl')
+
+
