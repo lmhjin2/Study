796d2646dea437e2dacc41924f90a3b3491160bb
lmhjin2 <lmhjin2@naver.com>
2024-03-28 15:22:11 +0900

    2024-03-28 15:22

--- tf114/tf13_mv2.py
+++ tf114/tf13_mv2.py
@@ -9,18 +9,20 @@ x_data = [[73,51,65],   # (5,3)
           [17,66,79]]
 y_data = [[152], [185], [180],[205],[142]] # (5,1)
 
-x = tf.compat.v1.placeholder(tf.float32, shape=[5,3])
-y = tf.compat.v1.placeholder(tf.float32, shape=[5,1])
+x = tf.compat.v1.placeholder(tf.float32, shape=[None,3])
+y = tf.compat.v1.placeholder(tf.float32, shape=[None,1])
 
-w = tf.compat.v1.Variable(tf.compat.v1.random_normal([1], dtype=tf.float32))
+w = tf.compat.v1.Variable(tf.compat.v1.random_normal([3, 1], dtype=tf.float32))
 b = tf.compat.v1.Variable(tf.compat.v1.random_normal([1], dtype=tf.float32))
+# x * w = y
+# (N, 3) * (3, 1) = (N, 1)
 
 #2 model
-hypothesis = x*w + b
+hypothesis = tf.matmul(x,w) + b
 
 #3 
 loss = tf.reduce_mean(tf.square(hypothesis - y))
-optimizer = tf.train.GradientDescentOptimizer(learning_rate=1e-4)
+optimizer = tf.train.GradientDescentOptimizer(learning_rate=8e-5)
 train = optimizer.minimize(loss)
 
 sess = tf.compat.v1.Session()
@@ -31,5 +33,4 @@ for step in range(epochs):
     _, loss_val = sess.run([train, loss], feed_dict={x:x_data, y:y_data})
     if step % 100 == 0: # step 을 100으로 나눴을때 나머지가 0이라면
         print(step, loss_val)
-
 
