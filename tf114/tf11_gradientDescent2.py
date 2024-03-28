6b5bd8f08f6e7bc4097a2f51323f93d9926266f8
lmhjin2 <lmhjin2@naver.com>
2024-03-28 12:29:38 +0900

    2024-03-28 12:29

---
+++ tf114/tf11_gradientDescent2.py
@@ -0,0 +1,63 @@
+import tensorflow as tf
+import matplotlib.pyplot as plt
+tf.set_random_seed(777)
+
+#1. 데이터
+x_train = [1,2,3]
+y_train = [1,2,3]
+
+x = tf.compat.v1.placeholder(tf.float32)
+y = tf.compat.v1.placeholder(tf.float32)
+
+w = tf.compat.v1.Variable([10], dtype=tf.float32, name='weight')
+b = tf.compat.v1.Variable([10], dtype=tf.float32, name='weight')
+
+#2. 모델
+hypothesis = x * w + b
+
+#3-1. 컴파일 // model.compile(loss='mse', optimizer='sgd')
+loss = tf.reduce_mean(tf.square(hypothesis-y)) # == mse
+
+###################### 옵티마이저 ###################################
+# optimizer = tf.train.GradientDescentOptimizer(learning_rate=0.08235)
+# train = optimizer.minimize(loss)
+
+lr = 0.1
+gradient = tf.reduce_mean((x * w + b - y) * x)
+descent = w - lr * gradient
+update = w.assign(descent)
+# w - lr * (( x * w - y) * x )
+# w - lr * (loss를 w로 미분한 값)
+###################### 옵티마이저 ###################################
+
+w_history = []
+loss_history=[]
+
+sess = tf.compat.v1.Session()
+sess.run(tf.compat.v1.global_variables_initializer())
+
+for step in range(35):
+    _, loss_v, w_v = sess.run([update, loss, w], feed_dict={x:x_train, y:y_train})
+    print(step, '\t', loss_v, '\t', w_v)
+    
+    w_history.append(w_v)
+    loss_history.append(loss_v)
+sess.close()
+
+print("="*50, 'W history', "="*50)
+print(w_history)
+print("="*50, 'L history', "="*50)
+print(loss_history)
+
+plt.plot(loss_history)
+plt.xlabel('Epochs')
+plt.ylabel('Loss')
+plt.show()
+
+plt.plot(w_history)
+plt.xlabel('Epochs')
+plt.ylabel('Weights')
+plt.show()
+
+
+
