9d760db009f802d37a7396dac45e0a638ed0e9b9
lmhjin2 <lmhjin2@naver.com>
2024-03-29 11:52:40 +0900

    2024-03-29 11:52

--- tf114/tf15_sigmoid1_만들기전.py
+++ tf114/tf15_sigmoid1_만들기전.py
@@ -1,6 +1,7 @@
 import tensorflow as tf
 tf.compat.v1.set_random_seed(777)
 
+#1. data
 x_data = [[1,2], [2,3], [3,1], [4,3], [5,3], [6,2]]  # (6,2)
 y_data = [[0], [0], [0], [1], [1], [1]]              # (6,1)
 
@@ -8,24 +9,59 @@ y_data = [[0], [0], [0], [1], [1], [1]]              # (6,1)
 ### [실습] 그냥한번 만들어봐
 #####################################################
 
+# x = tf.compat.v1.placeholder(tf.float32, shape=[None,2])
+# y = tf.compat.v1.placeholder(tf.float32, shape=[None,1])
+
+# w = tf.compat.v1.Variable(tf.compat.v1.random_normal([2,1], name='weights'))
+# b = tf.compat.v1.Variable(tf.compat.v1.zeros([1], name='bias'))
+# #2. model
+# hypothesis = tf.matmul(x,w) + b
+# #3-1. compile
+# loss = tf.reduce_mean(tf.square(hypothesis - y))
+# optimizer = tf.train.AdamOptimizer(learning_rate=1)
+# train = optimizer.minimize(loss)
+
+# sess = tf.compat.v1.Session()
+# sess.run(tf.compat.v1.global_variables_initializer())
+# #3-2. train
+# epochs = 30001
+# for step in range(epochs):
+#     _, val_loss = sess.run([train, loss], feed_dict={x:x_data, y:y_data})
+#     if step % 100 == 0:
+#         print(step, val_loss)
+
+#####################################################
+### [쌤코드]
+#####################################################
+#1. data
 x = tf.compat.v1.placeholder(tf.float32, shape=[None,2])
 y = tf.compat.v1.placeholder(tf.float32, shape=[None,1])
 
-w = tf.compat.v1.Variable(tf.compat.v1.random_normal([2,1], name='weights'))
-b = tf.compat.v1.Variable(tf.compat.v1.zeros([1], name='bias'))
+w = tf.compat.v1.Variable(tf.compat.v1.random_normal([2,1], dtype=tf.float32))
+b = tf.compat.v1.Variable(tf.compat.v1.zeros([1], dtype=tf.float32))
+#2. model
+hypothesis = tf.compat.v1.matmul(x,w) + b
 
-hypothesis = tf.matmul(x,w) + b
-
-loss = tf.reduce_mean(tf.square(hypothesis - y))
-optimizer = tf.train.AdamOptimizer(learning_rate=1)
+#3-1. compile
+loss = tf.reduce_mean(tf.compat.v1.square(hypothesis-y))
+optimizer = tf.train.GradientDescentOptimizer(learning_rate=1e-5)
 train = optimizer.minimize(loss)
 
+#3-2 train
 sess = tf.compat.v1.Session()
 sess.run(tf.compat.v1.global_variables_initializer())
 
 epochs = 30001
 for step in range(epochs):
-    _, val_loss = sess.run([train, loss], feed_dict={x:x_data, y:y_data})
+    loss_val, _, w_val, b_val = sess.run([loss, train, w, b],
+                                         feed_dict={x:x_data, y:y_data})
     if step % 100 == 0:
-        print(step, val_loss)
+        print(step, "loss : ", loss_val)
+
+print(w_val)
+# print(type(w_val))  # <class 'numpy.ndarray'>
+## sess.run 을 통과해서 나온 값은 모두 'numpy' 형태다
+
+
+
 
