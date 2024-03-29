e69f54764667bb5af71e19900a2fea767fb27851
lmhjin2 <lmhjin2@naver.com>
2024-03-29 15:54:38 +0900

    2024-03-29 15:54

--- dacon/tuning/tuning_grid.py
+++ dacon/tuning/tuning_grid.py
@@ -12,12 +12,12 @@ y = data['login']
 
 # GridSearchCV를 위한 하이퍼파라미터 설정
 param_search_space = {
-    'n_estimators': [110,549],
+    'n_estimators': [106,107,108,109,110,111,112,113],
     # 'criterion' : ['gini'],
     'max_depth': [None],
-    'min_samples_split': [2,6],   # 2이상의 정수 또는 0과 1사이의 실수(비율)
-    'min_samples_leaf': [8,4],    # 1이상의 정수 또는 0과 0.5 사이의 실수(비율)
-    'min_weight_fraction_leaf' : [0.0, 0.01, 0.011, 0.0105],     # 0.0 ~ 0.5 실수
+    'min_samples_split': [2],   # 2이상의 정수 또는 0과 1사이의 실수(비율)
+    'min_samples_leaf': [8],    # 1이상의 정수 또는 0과 0.5 사이의 실수(비율)
+    'min_weight_fraction_leaf' : [0.0, 0.0115],     # 0.0 ~ 0.5 실수
     # 'max_features' : ['auto']               # https://dacon.io/competitions/official/236229/data
     # 'max_leaf_nodes' : [None],              # None 또는 양의 정수
     # 'min_impurity_decrease' : [0.0],          # 0.0 이상의 실수
