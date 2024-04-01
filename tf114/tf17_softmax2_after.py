dc3943d0a4a6caf102c794afa5e9e8480a5335a2
lmhjin2 <lmhjin2@naver.com>
2024-04-01 17:05:49 +0900

    2024-04-01 17:05

--- tf114/tf17_softmax2_after.py
+++ tf114/tf17_softmax2_after.py
@@ -1,8 +1,10 @@
 import tensorflow as tf
 import numpy as np
-tf.compat.v1.set_random_seed(777)
+tf.set_random_seed(777)
 
-#1 data
+
+# 1. data
+
 x_data = [[1,2,1,1],
           [2,1,3,2],
           [3,1,3,4],
@@ -11,65 +13,50 @@ x_data = [[1,2,1,1],
           [1,2,5,6],
           [1,6,6,6],
           [1,7,6,7]]
-y_data = [[0,0,1], # 2
+
+y_data = [[0,0,1],
           [0,0,1],
           [0,0,1],
-          [0,1,0], # 1
           [0,1,0],
           [0,1,0],
-          [1,0,0], # 0
+          [0,1,0],
+          [1,0,0],
           [1,0,0]]
 
-x = tf.compat.v1.placeholder(dtype=tf.float32, shape=[None,4])
-y = tf.compat.v1.placeholder(dtype=tf.float32, shape=[None,3])
+x = tf.compat.v1.placeholder(tf.float32, shape=[None,4])
+w = tf.compat.v1.Variable(tf.random_normal([4,3]), name='weight')   # None,4 - None,3
+b = tf.compat.v1.Variable(tf.zeros([1,3]), name='bias') #4,3 - None,3
+y = tf.compat.v1.placeholder(tf.float32, shape=[None,3])
 
-w = tf.compat.v1.Variable(tf.compat.v1.random_normal(dtype=tf.float32, shape=[4,3]))
-b = tf.compat.v1.Variable(tf.compat.v1.zeros([1, 3]), name='bias')
+# 2. model
+hypothesis = tf.nn.softmax(tf.compat.v1.matmul(x,w) + b)
 
-#2 model
-hypothesis = tf.nn.softmax(tf.compat.v1.matmul(x, w) + b)
+# 3-1. compile
+loss = tf.reduce_mean(-tf.reduce_sum(y*tf.log(hypothesis),axis=1))  #categorical
 
-#3-1 compile
-loss = tf.reduce_mean( -tf.reduce_sum(y * tf.log(hypothesis), axis=1)) # categorical_crossentropy
-optimizer = tf.compat.v1.train.AdamOptimizer(learning_rate=1e-3)
-train = optimizer.minimize(loss)
-# 두줄에서 한줄로 만들기.
-# train = tf.compat.v1.train.GradientDescentOptimizer(learning_rate=1e-4).minimize(loss)
+# optimizer = tf.compat.v1.train.GradientDescentOptimizer(learning_rate=1e-4)
+# train = optimizer.minimize(loss)
+# ===똑같음
+train = tf.compat.v1.train.GradientDescentOptimizer(learning_rate=1e-1).minimize(loss)
 
-#[실습]
+sess = tf.compat.v1.Session()
+sess.run(tf.compat.v1.global_variables_initializer())
 
-#3-2 train
-with tf.compat.v1.Session() as sess:
-    sess.run(tf.compat.v1.global_variables_initializer())
+epochs = 1001
+for step in range(epochs):
+    cost_val, _, w_val, b_val = sess.run([loss,train,w,b],
+                                         feed_dict={x:x_data, y:y_data})
+    if step %20 == 0:
+        print(step, "loss : ", cost_val)
 
-    epochs = 24151
-    for step in range(epochs):
-        _, loss_val, w_val, b_val = sess.run([train, loss, w, b], feed_dict={x:x_data, y:y_data})
-        if step % 1 == 0 or epochs - 1:
-            print(step, loss_val)
 
-with tf.compat.v1.Session() as sess : 
-    sess.run(tf.compat.v1.global_variables_initializer())
-    a = sess.run(hypothesis, feed_dict={x: [[1, 11, 7, 9]]})
-    print(a, sess.run(tf.arg_max(a, 1)))
-    print('--------------------')
-    b = sess.run(hypothesis, feed_dict={x: [[1, 3, 4, 3]]})
-    print(b, sess.run(tf.arg_max(b, 1)))
-    print('--------------------')
-    c = sess.run(hypothesis, feed_dict={x: [[1, 1, 0, 1]]})
-    print(c, sess.run(tf.arg_max(c, 1)))
-    print('--------------------')
-    # all = sess.run(hypothesis, feed_dict={x: [[1, 11, 7, 9], [1, 3, 4, 3], [1, 1, 0, 1]]})
-    all = sess.run(hypothesis, feed_dict={x:[
-          [1,2,1,1],
-          [2,1,3,2],
-          [3,1,3,4],
-          [4,1,5,5],
-          [1,7,5,5],
-          [1,2,5,6],
-          [1,6,6,6],
-          [1,7,6,7]]})
-    
-    print(all, sess.run(tf.arg_max(all, 1)))
-
+pred = sess.run(hypothesis, feed_dict={x:x_data})
+print(pred) # 8행 3열
+pred = sess.run(tf.argmax(pred,axis=1))
+print(pred)
+y_data = np.argmax(y_data, axis=1)
+print(y_data)
+from sklearn.metrics import accuracy_score
+acc = accuracy_score(y_data,pred)
+print("acc : ", acc)
 sess.close()
