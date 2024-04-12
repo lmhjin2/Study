8d0fa485ba02a3d49185a528f2c7ca55504712dd
lmhjin2 <lmhjin2@naver.com>
2024-04-12 10:00:26 +0900

    2024-04-12 10:00

--- tf114/tf25_cnn1.py
+++ tf114/tf25_cnn1.py
@@ -13,5 +13,11 @@ y_test = to_categorical(y_test)
 x_train = x_train.reshape(60000,28,28,1).astype('float32')/255.
 x_test = x_test.reshape(10000,28,28,1).astype('float32')/255.
 
+#2 model
+x = tf.compat.v1.placeholder(tf.float32, shape = [None, 28,28,1])
+y = tf.compat.v1.placeholder(tf.float32, shape = [None, 10])
 
-
+w1 = tf.compat.v1.get_variable('w1', shape = [2, 2, 1, 64])
+                                      # 커널사이즈, 컬러(채널), 필터(아웃풋)
+L1 = tf.nn.conv2d(x, w1, strides=[1,1,1,1], padding='VALID') # 4차원이라서 [1,1,1,1], 중간 두개가 stride고 앞뒤의 1은 그냥 shape 맞춰주는놈임. 두칸씩 하려면 [1,2,2,1]. 2번이 가로 3번이 세로
+# model.add(Conv2D(64, kernel_size=(2,2), input_shape=(28,28,1), stride=(1,1)))
