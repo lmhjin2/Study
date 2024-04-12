e84a66e0ae8e613c4f559726e27035833458018a
lmhjin2 <lmhjin2@naver.com>
2024-04-12 09:42:24 +0900

    2024-04-12 09:42

---
+++ tf114/tf25_cnn1.py
@@ -0,0 +1,17 @@
+import tensorflow as tf
+import numpy as np
+tf.compat.v1.set_random_seed(777)
+
+#1 data
+from tensorflow.keras.datasets import mnist
+(x_train, y_train), (x_test, y_test) = mnist.load_data()
+
+from tensorflow.keras.utils import to_categorical
+y_train = to_categorical(y_train)
+y_test = to_categorical(y_test)
+
+x_train = x_train.reshape(60000,28,28,1).astype('float32')/255.
+x_test = x_test.reshape(10000,28,28,1).astype('float32')/255.
+
+
+
