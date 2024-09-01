200a65797c349c70ad46aa69b5d2bf45e7ca52b7
lmhjin2 <lmhjin2@naver.com>
2024-09-01 18:21:58 +0900

    2024-09-01 18:21

--- dacon/medicine/medicine03_CM.py
+++ dacon/medicine/medicine03_CM.py
@@ -8,9 +8,16 @@ from rdkit.Chem import AllChem
 from rdkit.Chem.rdMolDescriptors import GetMorganFingerprintAsBitVect
 from rdkit.Chem import rdFingerprintGenerator  # MorganGenerator를 사용하기 위해 추가
 from sklearn.model_selection import train_test_split
-from sklearn.ensemble import RandomForestRegressor
 from sklearn.metrics import mean_squared_error
+from sklearn.preprocessing import MinMaxScaler, MaxAbsScaler, StandardScaler, RobustScaler
 
+from keras.models import Sequential
+from keras.layers import *
+from keras.callbacks import EarlyStopping, ModelCheckpoint
+from keras.optimizers import *
+from keras.callbacks import ModelCheckpoint, EarlyStopping, ReduceLROnPlateau
+
+#1
 CFG = {
     'NBITS': 2048,
     'SEED': 42,
@@ -22,7 +29,6 @@ def seed_everything(seed):
     np.random.seed(seed)
 seed_everything(CFG['SEED'])  # Seed 고정
 
-# MorganGenerator 생성
 morgan_generator = rdFingerprintGenerator.GetMorganGenerator(radius=2, fpSize=CFG['NBITS'])
 
 # SMILES 데이터를 분자 지문으로 변환
@@ -34,10 +40,8 @@ def smiles_to_fingerprint(smiles):
     else:
         return np.zeros((CFG['NBITS'],))
 
-# 학습 ChEMBL 데이터 로드
 chembl_data = pd.read_csv('c:/data/dacon/medicine/train.csv')
 chembl_data.head()
-
 # print(chembl_data.shape) # (1952, 15) 
 
 train = chembl_data[['Smiles', 'pIC50']].copy()  # .copy()를 사용하여 경고 해결
@@ -46,36 +50,68 @@ train.loc[:, 'Fingerprint'] = train['Smiles'].apply(smiles_to_fingerprint)  # .l
 train_x = np.stack(train['Fingerprint'].values)
 train_y = train['pIC50'].values
 
-# 학습 및 검증 데이터 분리
-train_x, val_x, train_y, val_y = train_test_split(train_x, train_y, test_size=0.2, random_state=42)
+train_x, val_x, train_y, val_y = train_test_split(train_x, train_y, test_size=0.18, random_state=42)
 
-print(train_x.shape, train_y.shape)   # (1561, 2048) / (1561,)
-print(val_x.shape, val_y.shape)       # (391, 2048)  / (391,)
+# print(train_x.shape, train_y.shape)   # (1561, 2048) / (1561,)
+# print(val_x.shape, val_y.shape)       # (391, 2048)  / (391,)
 
-# # 랜덤 포레스트 모델 학습
-# model = RandomForestRegressor(random_state=CFG['SEED'])
-# model.fit(train_x, train_y)
+#2
+model = Sequential()
+model.add(Dense(64, input_shape=(2048,)))
+model.add(Dropout(0.2))
+model.add(Dense(128))
+model.add(Dropout(0.2))
+model.add(Dense(256, activation='relu'))
+model.add(BatchNormalization())
+model.add(Dense(512))
+model.add(Dropout(0.2))
+model.add(Dense(256, activation='relu'))
+model.add(BatchNormalization())
+model.add(Dense(128))
+model.add(Dropout(0.2))
+model.add(Dense(64, activation='relu'))
+model.add(BatchNormalization())
+model.add(Dense(32))
+model.add(Dropout(0.2))
+model.add(Dense(16, activation='relu'))
+model.add(BatchNormalization())
+model.add(Dense(8))
+model.add(Dropout(0.2))
+model.add(Dense(4, activation='relu'))
+model.add(BatchNormalization())
+model.add(Dense(1))
 
-# def pIC50_to_IC50(pic50_values):
-#     """Convert pIC50 values to IC50 (nM)."""
-#     return 10 ** (9 - pic50_values)
+#3
+model.compile(loss='mse', optimizer=Adam(learning_rate=0.001))
 
-# # Validation 데이터로부터의 학습 모델 평가
-# val_y_pred = model.predict(val_x)
-# mse = mean_squared_error(pIC50_to_IC50(val_y), pIC50_to_IC50(val_y_pred))
-# rmse = np.sqrt(mse)
+es = EarlyStopping(monitor='mse', mode='min', verbose=1, patience=100, restore_best_weights=True)
 
-# print(f'RMSE: {rmse}')
+hist = model.fit(train_x, train_y, epochs = 2000,
+                 batch_size=352, validation_split=0.12, verbose=1)
 
-# test = pd.read_csv('c:/data/dacon/medicine/test.csv')
-# test.loc[:, 'Fingerprint'] = test['Smiles'].apply(smiles_to_fingerprint)
+#4
+def pIC50_to_IC50(pic50_values):
+    """Convert pIC50 values to IC50 (nM)."""
+    return 10 ** (9 - pic50_values)
 
-# test_x = np.stack(test['Fingerprint'].values)
+loss = model.evaluate(val_x, val_y)
 
-# test_y_pred = model.predict(test_x)
+# Validation 데이터로부터의 학습 모델 평가
+val_y_pred = model.predict(val_x)
+mse = mean_squared_error(pIC50_to_IC50(val_y), pIC50_to_IC50(val_y_pred))
+rmse = np.sqrt(mse)
 
-# submit = pd.read_csv('c:/data/dacon/medicine/sample_submission.csv')
-# submit['IC50_nM'] = pIC50_to_IC50(test_y_pred)
-# submit.head()
+print(f'RMSE: {rmse}')
 
-# submit.to_csv('c:/data/dacon/medicine/submission/use_MG.csv', index=False)
+# test.csv 데이터로부터의 학습 모델 평가
+test = pd.read_csv('c:/data/dacon/medicine/test.csv')
+test.loc[:, 'Fingerprint'] = test['Smiles'].apply(smiles_to_fingerprint)
+test_x = np.stack(test['Fingerprint'].values)
+
+test_y_pred = model.predict(test_x)
+submit = pd.read_csv('c:/data/dacon/medicine/sample_submission.csv')
+submit['IC50_nM'] = pIC50_to_IC50(test_y_pred)
+submit.head()
+
+submit.to_csv('c:/data/dacon/medicine/submission/03_CM.csv', index=False)
+# https://dacon.io/competitions/official/236336/mysubmission
